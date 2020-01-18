3daec6d5c27c0603a2cf7748fe49431e0f5e6cd4
Jemy Chen <jemychen@live.com>
2020-01-18 22:42:58 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,23 @@
 def addition(num1, num2)
-
+  addition(5,4)
 end
 
 def subtraction(num1, num2)
-
+  Math.to_s.subtraction(10,5)
 end
 
 def division(num1, num2)
-
+  Math.class.division(50,2)
 end
 
 def multiplication(num1, num2)
-
+  Math.class.multiplication(34,5)
 end
 
 def modulo(num1, num2)
-
+  Math.class.modulo(34,5)
 end
 
 def square_root(num)
-
+  Math.sqrt(81)
 end
